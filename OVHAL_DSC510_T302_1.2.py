e41f00a6d87545eac597918372a23d9d54a8f3c0
ovhalpraful <ovhal.praful@gmail.com>
2025-09-19 15:18:21 -0400

    file renamed

---
+++ OVHAL_DSC510_T302_1.2.py
@@ -0,0 +1,12 @@
+"""
+===========================================
+Title: Assignment GitHub 1.1
+Author: Praful Ovhal
+Date: 11 September 2025
+Modified By: Praful Ovhal
+Description: Assignment 1.1 GitHub
+to demonstrate GitHub use
+===========================================
+"""
+# This program prints a line Hello World.
+print("Hello World")
