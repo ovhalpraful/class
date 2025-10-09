79a09f1c609bd219b81206d9802955fb26f40b2e
ovhalpraful <47410535+ovhalpraful@users.noreply.github.com>
2025-10-09 17:08:34 -0400

    changes

--- OVHAL_DSC510_T302_W5_5.1.py
+++ OVHAL_DSC510_T302_W5_5.1.py
@@ -6,6 +6,7 @@
 #Date 10-08-2025
 #--------------------------------------------------------------
 
+# Function to calculate math operations.
 def perform_calculations(operation):
     try:
         num1 = float(input('\n Please enter first number: '))
@@ -28,6 +29,7 @@ def perform_calculations(operation):
         print("\n Please enter a valid number and try again.")
         return None
 
+# Function to calculate average
 def calculate_average():
     try:
         number_range = int(input('\n Please enter total count of numbers: '))
@@ -36,7 +38,7 @@ def calculate_average():
             print("\n Please enter a number greater than zero and try again.")
 
         total = 0.0
-        for count in range(int(number_range)):
+        for count in range(int(number_range)):      #Looping through the range & adding the numbers
             while True:
                 try:
                     number = float(input(f"\n Please enter {count+1} number: "))
@@ -59,7 +61,7 @@ def main():
         print("\n 1.Addition \n 2.Subtraction \n 3.Multiplication \n 4.Division \n 5.Average \n 6.Exit ")
 
         try:
-            choice = input('\n Please enter your choice as (1 or 2 or 3 or 4 or 5 or 6): ').strip()
+            choice = int(input("\n Please enter your choice as (1 or 2 or 3 or 4 or 5 or 6): "))
 
             if choice == '1':
                 result = perform_calculations('+')
@@ -85,6 +87,12 @@ def main():
                 print("\n Invalid choice. Please try again.")
         except ValueError:
             print("\n Please enter a valid choice and try again.")
+        # Handling the exception while terminating the program
+        except KeyboardInterrupt:
+            print("\n Exiting program...")
+            exit()
+        finally:
+            print("\n Thank you for using Python Calculator")
 
 if __name__ == '__main__':
     main()
