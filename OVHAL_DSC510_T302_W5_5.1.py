4f81104cce16ff20308b757c2d5b9426efe1cbf4
ovhalpraful <47410535+ovhalpraful@users.noreply.github.com>
2025-10-09 15:29:22 -0400

    Update OVHAL_DSC510_T302_W5_5.1.py

--- OVHAL_DSC510_T302_W5_5.1.py
+++ OVHAL_DSC510_T302_W5_5.1.py
@@ -7,40 +7,83 @@
 #--------------------------------------------------------------
 
 def perform_calculations(operation):
-    operation = input('\n What kind of calculation do you want to perform? Please enter the symbol: \n 1. for Add + \n 2.for Subtract - \n 3. for Multiply * \n 4.for Divide / \n >>')
-    operand_1 = float(input('\n Please enter first operand? : '))
-    operand_2 = float(input('\n Please enter second operand? : '))
-    
-    if operation == '+':
-        addition = operand_1 + operand_2
-        print(f"\n Addition result: {addition}")
-        exit()
-    if operation == '-':
-        substraction = operand_1 - operand_2
-        print(f"\n Substraction result: {addition}")
-        exit()
-    if operation == '*':
-        multiplication = operand_1 * operand_2
-        print(f"\n Multiplication result: {multiplication}")
-        exit()
-    if operation == '/':
-        division = operand_1 / operand_2
-        print(f"\n Division result: {division}")
-        exit()
+    try:
+        num1 = float(input('\n Please enter first number: '))
+        num2 = float(input('\n Please enter second number: '))
+
+        if operation == '+':
+            return num1 + num2
+        elif operation == '-':
+            return num1 - num2
+        elif operation == '*':
+            return num1 * num2
+        elif operation == '/':
+            if num2 == 0:
+                print("\n Division by zero is not possible. Please try again.")
+            else:
+                return num1 / num2
+        else:
+            print("\n Operation not recognized. Please try again.")
+    except ValueError:
+        print("\n Please enter a valid number and try again.")
+        return None
 
 def calculate_average():
-    numbers = input('\n Please total number of numbers you want : ')
-    count = 0
-    values = []
+    try:
+        number_range = int(input('\n Please enter total count of numbers: '))
+
+        if number_range <= 0:
+            print("\n Please enter a number greater than zero and try again.")
+
+        total = 0
+        for count in range(int(number_range)):
+            while True:
+                try:
+                    number = float(input(f"\n Please enter {count+1} number: "))
+                    total = total + float(number)
+                    break
+                except ValueError:
+                    print("\n Please enter a valid number and try again.")
+
+        average = total / number_range
+        return average
+    except ValueError:
+        print("\n Please enter a valid number and try again.")
+        return None
 
 
 
 def main():
-    choice = input("\n Please select the operation that you wish to perform. \n For Calculation enter C and for Average enter A \n >>")
-    if choice == 'C':
-        perform_calculations()
-    if choice == 'A':
-        calculate_average()
+    print("\n Welcome to Python Calculator")
+
+    while True:
+        print("\n Choose an operation: ")
+        print("\n 1.Addition \n 2.Subtraction \n 3.Multiplication \n 4.Division \n 5.Average \n 6.Exit ")
+
+        try:
+            choice = input('\n Please enter your choice as (1 or 2 or 3 or 4 or 5 or 6): ').strip()
+
+            if choice == '1':
+                result = perform_calculations('+')
+                print(f"\n The addition result is: {result}")
+            elif choice == '2':
+                result = perform_calculations('-')
+                print(f"\n The subtraction result is: {result}")
+            elif choice == '3':
+                result = perform_calculations('*')
+                print(f"\n The multiplication result is: {result}")
+            elif choice == '4':
+                result = perform_calculations('/')
+                print(f"\n The division result is: {result}")
+            elif choice == '5':
+                result = calculate_average()
+                print(f"\n The average result is: {result}")
+            elif choice == '6':
+                exit()
+            else:
+                print("\n Invalid choice. Please try again.")
+        except ValueError:
+            print("\n Please enter a valid choice and try again.")
 
 if __name__ == '__main__':
     main()
