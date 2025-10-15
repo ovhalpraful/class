57ab0222ea8501f10ac1fec0b45dd6bc10c7d507
ovhalpraful <ovhal.praful@gmail.com>
2025-10-15 14:51:03 -0400

    Update OVHAL_DSC510_T302_W5_6.1.py

--- OVHAL_DSC510_T302_W5_6.1.py
+++ OVHAL_DSC510_T302_W5_6.1.py
@@ -1,23 +1,25 @@
 #------------------------------------------------------------
 #DSC 510
 #Week 6
-#Programming Assignment 6.1 - Accepting temperatures and finding the smallest & largest
+#Programming Assignment 6.1 - Accepting temperatures and finding the smallest & largest in the list
 #Author Praful Ovhal
 #Date 10-14-2025
 from decorator import append
 
 
 #Change#: 1
-#Change(s) Made:
-#Date of Change:
+#Change(s) Made: Added comments and formatted the output
+#Date of Change: 10-15-2025
 #Author: Praful Ovhal
 #Change Approved by: Praful Ovhal
-#Date Moved to Production:
+#Date Moved to Production: 10-15-2025
 #--------------------------------------------------------------
 
 def main():
     sentinel = 'q'
     temperature = []
+
+    #Accepting temperatures from the user
     while True:
         user_input = input("Enter the temperature in fahrenheit OR Press q to quit: ")
 
@@ -32,15 +34,17 @@ def main():
             print("Exiting the program...")
             break
 
-    print("\n Here are the temperatures you have entered:", temperature)
-
-    maximum_temp = max(temperature)
-    minimum_temp = min(temperature)
-    count = len(temperature)
-
-    print("\n You have entered %d temperature value(s)" % count)
-    print("\n The maximum temperature in the list is %g\u00B0F" % maximum_temp)
-    print("\n The minimum temperature in the list is %g\u00B0F" % minimum_temp)
+    #Finding the minimum, maximum, & total count of temperature in the list
+    if temperature:
+        maximum_temp = max(temperature)
+        minimum_temp = min(temperature)
+        count = len(temperature)
+        print("\nYou have entered %d temperature value(s)" % count)
+        print("Here are the temperatures you have entered:", [round(temperature, 2) for temperature in temperature])
+        print(f"The maximum temperature in the list is", format(maximum_temp, '.2f'),"\u00B0F")
+        print(f"The minimum temperature in the list is", format(minimum_temp, '.2f'),"\u00B0F")
+    else:
+        print("No temperature(s) entered. Thank you for using this program.")
 
 if __name__ == '__main__':
     main()
