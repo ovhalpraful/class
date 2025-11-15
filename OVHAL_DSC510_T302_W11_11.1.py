1d35921f5a27c954ea8546b9bb04895f6de17d0a
ovhalpraful <47410535+ovhalpraful@users.noreply.github.com>
2025-11-14 22:16:40 -0500

    Update OVHAL_DSC510_T302_W11_11.1.py

--- OVHAL_DSC510_T302_W11_11.1.py
+++ OVHAL_DSC510_T302_W11_11.1.py
@@ -15,9 +15,11 @@
 import requests
 import json
 import logging
+from tabulate import tabulate
 
 country_code = 'US'
 api_key = "f577dc33e820ff8ed415cd0b7189ca33"
+unit = "imperial"
 
 # Error logging
 logging.basicConfig(
@@ -36,7 +38,7 @@ def get_coordinates(api_coordinates_url):
 def get_weather(coordinates):
     latitude = coordinates['lat']
     longitude = coordinates['lon']
-    api_weather_url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}"
+    api_weather_url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}&units={unit}"
     headers = {'cache-control': 'no-cache'}
     response = requests.get(api_weather_url, headers=headers)
     weather = json.loads(response.text)
@@ -44,13 +46,19 @@ def get_weather(coordinates):
 
 def print_weather(weather):
     print("-"*40)
+    print(f"{'Parameters':<30}{'Values':<60}")
+    print("-" * 40)
     print(f"{'City':<30}{weather["name"]:<60}")
     print(f"{'Cloud Coverage':<30}{weather["weather"][0]["main"]:<60}")
-    print(f"{'Temperature':<30}{weather["main"]["temp"]:<60}")
-    print(f"{'Max Temperature':<30}{weather["main"]["temp_max"]:<60}")
-    print(f"{'Min Temperature':<30}{weather["main"]["temp_min"]:<60}")
-    print(f"{'Pressure':<30}{weather["main"]["pressure"]:<60}")
-    print(f"{'Humidity':<30}{weather["main"]["humidity"]:<60}")
+    print(f"{'Description':<30}{weather["weather"][0]["description"]:<60}")
+    print(f"{'Current Temperature':<30}{weather["main"]["temp"]:<60} F")
+    print(f"{'Max Temperature':<30}{weather["main"]["temp_max"]:<60} F")
+    print(f"{'Min Temperature':<30}{weather["main"]["temp_min"]:<60} F")
+    print(f"{'Feels Like':<30}{weather["main"]["feels_like"]:<60}")
+    print(f"{'Wind Speed':<30}{weather["wind"]["speed"]:<60} m/h")
+    print(f"{'Pressure':<30}{weather["main"]["pressure"]:<60} psi")
+    print(f"{'Humidity':<30}{weather["main"]["humidity"]:<60} %")
+    print(f"{'Visibility':<30}{weather["visibility"]:<60} mi\"")
     print("-"*40)
 
 def main():
